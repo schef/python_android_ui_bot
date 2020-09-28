8456fa24e1288b681807ec5145864954365f8a56
schef <zvanstefan@gmail.com>
2020-09-28 21:28:42 +0200

    playing with swipes

--- uiautomator2/insta_like.py
+++ uiautomator2/insta_like.py
@@ -38,10 +38,10 @@ def pressMenu(device):
     device.press("menu")
 
 def scrollDown(device):
-    device(scrollable=True).scroll.vert.forward(steps=50)
+    device.swipe_ext("up", scale=0.3)
 
 def scrollUp(device):
-    device(scrollable=True).scroll.vert.backward(steps=50)
+    device.swipe_ext("down", scale=0.3)
     
 def scrollTop(device):
     device(scrollable=True).scroll.toBeginning()
@@ -83,6 +83,106 @@ def appAutoRestart(device):
     appAutoResume(device)
 
 ##### APP INFO end #####
+##### HOME start #####
+
+def isHome(device):
+    return device.xpath("//android.widget.FrameLayout[@content-desc='Home']").info["selected"] == "true"
+
+def homeGo(device):
+    device.xpath("//android.widget.FrameLayout[@content-desc='Home']").click()
+    
+def getWidgetY(widget):
+    return widget.center()[1]
+
+def getProfiles(device):
+    profiles = device(className="android.widget.TextView", resourceId="com.instagram.android:id/row_feed_photo_profile_name")
+    if (len(profiles)):
+        return profiles
+    return []
+
+def getTopProfile(device):
+    profiles = getProfiles(device)
+    if len(profiles):
+        return profiles[0]
+    return None
+
+def getNextProfile(device, profile):
+    for widget in getProfiles(device):
+        if getWidgetY(widget) > getWidgetY(profile):
+            return widget 
+    return None
+
+def getProfileName(profile):
+    return profile.info["text"]
+
+def swipeProfileToTop(device, profile):
+    HEADER_OFFSET = 150
+    profile.swipe_ext("up", box=(100, 500, 100, 500+))
+
+def isProfileSponsored(profile):
+    sibling = profile.sibling(text="Sponsored")
+    if (len(sibling)):
+        return True
+    return False
+
+def printProfileNames(device, num):
+    currentView = None
+    for i in range(num):
+        view = getTopProfile(device)
+        print(view)
+        if (view != currentView):
+            print(getProfileName(view))
+            currentView = view
+        scrollDown(device)
+        
+
+def getTopButtons(device):
+    return device.xpath("//*[@resource-id='com.instagram.android:id/row_feed_view_group_buttons']")
+
+def getButtonsBelowProfile(device, profile):
+    for widget in device.xpath("//*[@resource-id='com.instagram.android:id/row_feed_view_group_buttons']").all():
+        if getWidgetY(widget) > getWidgetY(profile):
+            return widget 
+    return None
+    
+
+##### HOME end #####
+##### SEARCH AND EXPLORE start #####
+
+def isSearchAndExplore(device):
+    return device.xpath("//android.widget.FrameLayout[@content-desc='Search and Explore']").info["selected"] == "true"
+
+def searchAndExploreGo(device):
+    device.xpath("//android.widget.FrameLayout[@content-desc='Search and Explore']").click()
+
+##### SEARCH AND EXPLORE end #####
+##### CAMERA start #####
+
+def isCamera(device):
+    return device.xpath("//android.widget.FrameLayout[@content-desc='Camera']").info["selected"] == "true"
+
+def cameraGo(device):
+    device.xpath("//android.widget.FrameLayout[@content-desc='Camera']").click()
+
+##### CAMERA end #####
+##### ACTIVITY start #####
+
+def isActivity(device):
+    return device.xpath("//android.widget.FrameLayout[@content-desc='Activity']").info["selected"] == "true"
+
+def activityGo(device):
+    device.xpath("//android.widget.FrameLayout[@content-desc='Activity']").click()
+
+##### ACTIVITY end #####
+##### PROFILE start #####
+
+def isProfile(device):
+    return device.xpath("//android.widget.FrameLayout[@content-desc='Profile']").info["selected"] == "true"
+
+def profileGo(device):
+    device.xpath("//android.widget.FrameLayout[@content-desc='Profile']").click()
+
+##### PROFILE end #####
 ##### BLOGGER start #####
 
 def getUserName(device):
